3942d030d3dbbfbbc18ed5b27c06af3fc6f557b3
sakshimehta12 <sakshimehta.010191@gmail.com>
2025-03-27 23:12:23 +0530

    init commit

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter your name:");
-        String name=sc.nextLine();
+        String name = sc.nextLine();
 
         System.out.println("Enter your roll no:");
-        int roll_no=sc.nextInt();
+        int roll_no = sc.nextInt();
 
-        System.out.println("My name is " + name+ " and Roll no is "+ roll_no);
+        System.out.println("My name is " + name + " and Roll no is " + roll_no);
 
         sc.close();
     }
