06f1c4fbd3bcf81be8b3c31f3c37d1b548acf7dc
sakshimehta12 <sakshimehta.010191@gmail.com>
2025-03-27 23:08:56 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter your name:");
+        String name=sc.nextLine();
+
+        System.out.println("Enter your roll no:");
+        int roll_no=sc.nextInt();
+
+        System.out.println("My name is " + name+ " and Roll no is "+ roll_no);
+
+        sc.close();
+    }
+}
