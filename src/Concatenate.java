7d3ea67d13b578ce25fa48189817c08fed27200b
sakshimehta12 <sakshimehta.010191@gmail.com>
2025-04-11 23:57:59 +0530

    16 commit

---
+++ src/Concatenate.java
@@ -0,0 +1,8 @@
+public class Concatenate {
+    public static void main(String[] args) {
+        String firstString = "Hello, ";
+        String secondString = "world!";
+        String result = firstString + secondString;
+        System.out.println("Concatenated String: " + result);
+    }
+}
