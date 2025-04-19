c2d3ae4d2bc5c67a5029ae9d5e4a0e36a830834c
sakshimehta12 <sakshimehta.010191@gmail.com>
2025-04-19 23:19:52 +0530

    24 commit

---
+++ src/EvenNumbers.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class EvenNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        System.out.println("Even numbers in the array:");
+        for (int i = 0; i < n; i++) {
+            if (arr[i] % 2 == 0) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+
+        scanner.close();
+    }
+}
